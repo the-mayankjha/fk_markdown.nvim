f3947ebba0b4dbfc40eecad64dcb43db02eaa77d
Mayank Jha <the.mayank.k.jha@gmail.com>
2026-08-30 11:14:29 +0530

    Stop tracking patches

--- fix_server.py
+++
@@ -1,7 +0,0 @@
-with open('lua/fk_markdown/preview/server.lua', 'r') as f:
-    content = f.read()
-
-content = content.replace("else\n                    \n                else\n                    -- attempt to serve local file", "else\n                    -- attempt to serve local file")
-
-with open('lua/fk_markdown/preview/server.lua', 'w') as f:
-    f.write(content)
